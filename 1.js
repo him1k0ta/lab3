27bd4d478473003f7b843e3f5ffbec8de13396c2
him1k0ta <149035975+him1k0ta@users.noreply.github.com>
2024-01-28 23:29:47 +0700

    my code1

---
+++ 1.js
@@ -0,0 +1,12 @@
+/*
+*Кайль Ангелина
+*/
+
+alert("Программа вычисляющая среднее арифметическое");
+alert("Оба числа > 0");
+var numberOne = prompt("Введите первое число: ");
+var numberTwo = prompt("Введите второе число: ");
+numberOne = parseFloat(numberOne);
+numberTwo = parseFloat(numberTwo);
+var resultat = (numberOne + numberTwo) / 2;
+alert(resultat)
